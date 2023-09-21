f22ee185627902bb3caa10474cc69016343892a1
DMergy <mer21040@byui.edu>
2023-09-20 20:40:37 -0700

    clean up

--- scripts/index.js
+++ scripts/index.js
@@ -1,3 +1,5 @@
+// Back to top button //
+
 const backToTop = document.querySelector(".back-to-top");
 const observerTarget = document.querySelector("header");
 
@@ -15,3 +17,32 @@ const observer = new IntersectionObserver((entries, observer) => {
 
 observer.observe(observerTarget);
 
+// footer dates //
+
+// select the elements to manipulate (output to)
+const datefield = document.querySelector(".date");
+const datefieldUK = document.querySelector("aside"); // for european/family history format with day first.
+
+// derive the current date using a date object
+const now = new Date();
+const fulldate = new Intl.DateTimeFormat("en-US", { dateStyle: "full" }).format(
+	now
+);
+const fulldateUK = new Intl.DateTimeFormat("en-UK", {
+	dateStyle: "full"
+}).format(now);
+// long, medium, short options ... try them
+
+// datefield.innerHTML = `<em>${fulldate}</em>`;
+datefieldUK.innerHTML = `<em>${fulldateUK}</em>`;
+
+const date = new Date();
+const year = date.getFullYear();
+
+document.querySelector("#currentYear").textContent = year;
+
+const lastMod = document.lastModified;
+document.querySelector("#lastMod").textContent = lastMod;
+
+// const options = {weekday: 'long', day: 'numeric', month: 'long', year: 'numeric'};
+// document.getElementById('currentdate').textContent = new Date().toLocaleDateString('en-US', options);
